1522964d5ca98968d088ab27904483fa6e61f089
김민호 <kr545@naver.com>
2022-03-11 20:31:02 +0900

    URL 별칭

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
+    path('<int:question_id>/',views.detail,name='detail'),
+    #http://localhost:8000/pybo/ URL은 index, http://localhost:8000/pybo/2와 같은 URL에는 detail 이라는 이름을 부여했다.
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('안녕하세요')
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date') # order_by = 정령 
+    context = {'question_list':question_list}
+    return render(request, 'pybo/question_list.html',context) # context 는 딕셔너리형으로 사용하며 key 값이 탬플릿에서 사용할 변수이름, value 값이 파이썬 변수
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
