9e6c5ece001f1a45d192ba690f8c9af1bb656bdf
김민호 <kr545@naver.com>
2022-03-15 17:24:50 +0900

    마크다운 기능추가

--- mysite/pybo/templatetags/pybo_filter.py
+++ mysite/pybo/templatetags/pybo_filter.py
@@ -1,11 +1,18 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
-
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
 
 # 템플릿 필터 함수를 만드는 방법은 무척 간단하다. 위처럼 sub 함수에 @register.filter 애너테이션을 적용하면 템플릿에서 해당 함수를 필터로 사용할 수 있게 된다.
 
